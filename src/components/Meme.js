a2518a7d4f0916cb0526b1d57ab95fb0377f2122
that devguy <zkmutch@gmail.com>
2024-01-16 15:00:54 -0500

    updated the state in preperation for saving the state in the remaining portions of the meme generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,12 +2,23 @@ import React from "react";
 import memesData from "../data";
 
 function Meme() {
-  const [memeImage, setMemeImage] = React.useState("");
+  // const [memeImage, setMemeImage] = React.useState("");
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
   function handleClick() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
 
   return (
@@ -21,7 +32,7 @@ function Meme() {
       <button type="submit" className="meme--button" onClick={handleClick}>
         Get a new meme image 🖼
       </button>
-      <img src={memeImage} className="meme--image" alt="Meme" />
+      <img src={meme.randomImage} className="meme--image" alt="Meme" />
     </div>
   );
 }
